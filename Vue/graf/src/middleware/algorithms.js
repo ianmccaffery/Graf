9de79973ff39578eed89fa03a4eae0fd1ae79630
DQian2001 <66427829+DQian2001@users.noreply.github.com>
2021-07-23 00:03:52 -0400

    added helperAlgs.maxFlow AND grafhelpers.BFS_maxFlow
    Implemented mechanics for the Maximum Flow algorithm, along with the BFS helper function that the Maximum Flow algorithm uses in order to find a new augmenting path.

--- Vue/graf/src/middleware/algorithms.js
+++ Vue/graf/src/middleware/algorithms.js
@@ -57,6 +57,107 @@ class helperAlgs {
         }
     }
 
+    maxFlow(selectedNodes, links) {
+        console.log("***** IN maxFlow *****");
+
+        console.log("Starting node is " + selectedNodes[0].index);
+
+        // ***** MODIFIED "convertGrafData" CODE *****
+        var data = {};
+        for(let edge in links) {
+            var lNode = links[edge].sid; // source node id
+            var rNode = links[edge].tid; // target node id
+            
+            // TODO: Error checking to ensure links[edge].name is a POSITIVE INTEGER
+            var weight = parseInt(links[edge].name); // edge weight int
+
+            if(links[edge].type === "Directed") {
+                if(lNode in data) {
+                    data[lNode][rNode] = weight;
+                } else {
+                    data[lNode] = {}; // initialize an empty object
+                    data[lNode][rNode] = weight;
+                }
+                if(!(rNode in data)) {
+                    data[rNode] = {}; // initialize an empty object
+                }
+            } else {
+                // TREAT UNDIRECTED AND BIDIRECTED EDGES THE SAME WAY
+                
+                // if there is a bidirected or undirected edge ...
+                // from A to B with weight w, then there is a directed edge ...
+                // from A to B with weight w AND a directed edge ...
+                // from B to A with weight w.
+                if(lNode in data) {
+                    data[lNode][rNode] = weight;
+                } else {
+                    data[lNode] = {}; // initialize an empty object
+                    data[lNode][rNode] = weight;
+                }
+
+                if(rNode in data) {
+                    data[rNode][lNode] = weight;
+                } else {
+                    data[rNode] = {}; // initialize an empty object
+                    data[rNode][lNode] = weight;
+                }
+            }
+        }
+        console.log("Graph data structure is...");
+        console.log(JSON.parse(JSON.stringify(data)));
+
+        var path = grafhelpers.BFS_maxFlow(selectedNodes, data);
+        var residual = data;
+
+        while(path.length > 0) {
+            var bottleneck = Infinity;
+            var node1;
+            var node2;
+            for(let n = 0; n < path.length - 1; n++) {
+                node1 = path[n];
+                node2 = path[n + 1];
+                if(residual[node1][node2] < bottleneck) {
+                    bottleneck = residual[node1][node2];
+                }
+            }
+            for(let n = 0; n < path.length - 1; n++) {
+                node1 = path[n];
+                node2 = path[n + 1];
+                residual[node1][node2] -= bottleneck;
+                if(residual[node1][node2] == 0) {
+                    delete residual[node1][node2];
+                }
+                if(node2 in residual) {
+                    if(node1 in residual[node2]) {
+                        residual[node2][node1] += bottleneck;
+                    }
+                    else {
+                        residual[node2][node1] = bottleneck;
+                    }
+                }
+                else {
+                    residual[node2] = {};
+                    residual[node2][node1] = bottleneck;
+                }
+            }
+            console.log("Residual graph is now...");
+            console.log(JSON.parse(JSON.stringify(residual)));
+            path = grafhelpers.BFS_maxFlow(selectedNodes, data);
+        }
+
+        console.log("Final graph is...");
+        console.log(data);
+
+        var source = selectedNodes[0].index;
+        var maxf = 0;
+        for(let n in residual) {
+            if(source in residual[n]) {
+                maxf += residual[n][source];
+            }
+        }
+        console.log("Maximum flow is " + maxf);
+        return maxf;
+    }
 }
 
 export default new helperAlgs();
